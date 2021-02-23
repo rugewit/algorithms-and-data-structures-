4ef90847e3fe193f9ec01050af36826816ae3343
rugewit <rugewit@yandex.ru>
2021-02-23 04:01:42 +0300

    end with btree

--- BTree/BTree.cpp
+++ BTree/BTree.cpp
@@ -110,7 +110,7 @@ public:
     void insert(int k);
 
     // The main function that removes a new key in thie B-Tree
-    void remove();
+    void remove(int k);
 };
 
 // Constructor for BTreeNode class 
@@ -279,10 +279,141 @@ void BTreeNode::fill(int idx) {
         borrowFromNext(idx);
     }
 
-
+    // Merge C[idx] with its sibling 
+    // If C[idx] is the last child, merge it with with its previous sibling 
+    // Otherwise merge it with its next sibling 
+    else {
+        if (idx != n) {
+            merge(idx);
+        }
+        else {
+            merge(idx - 1);
+        }
+    }
+    return;
 }
 
+// A function to borrow a key from C[idx-1] and insert it 
+// into C[idx] 
+void BTreeNode::borrowFromPrev(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx - 1];
 
+    // The last key from C[idx-1] goes up to the parent and key[idx-1] 
+    // from parent is inserted as the first key in C[idx]. Thus, the  loses 
+    // sibling one key and child gains one key 
+
+    // Moving all key in C[idx] one step ahead 
+    for (int i = child->n - 1; i >= 0; --i) {
+        child->keys[i + 1] = child->keys[i];
+    }
+
+    // If C[idx] is not a leaf, move all its child pointers one step ahead 
+    if (!child->leaf) {
+        for (int i = child->n; i >= 0; --i) {
+            child->C[i + 1] = child->C[i];
+        }
+    }
+
+    // Setting child's first key equal to keys[idx-1] from the current node 
+    child->keys[0] = keys[idx - 1];
+
+    // Moving sibling's last child as C[idx]'s first child 
+    if (!child->leaf) {
+        child->C[0] = sibling->C[sibling->n];
+    }
+
+    // Moving the key from the sibling to the parent 
+    // This reduces the number of keys in the sibling 
+    keys[idx - 1] = sibling->keys[sibling->n - 1];
+
+    child->n++;
+    sibling->n--;
+    return;
+}
+
+// A function to borrow a key from the C[idx+1] and place 
+// it in C[idx] 
+void BTreeNode::borrowFromNext(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx + 1];
+
+    // keys[idx] is inserted as the last key in C[idx] 
+    child->keys[(child->n)] = keys[idx];
+
+    // Sibling's first child is inserted as the last child 
+    // into C[idx] 
+    if (!child->leaf) {
+        child->C[child->n + 1] = sibling->C[0];
+    }
+
+    //The first key from sibling is inserted into keys[idx] 
+    keys[idx] = sibling->keys[0];
+
+    // Moving all keys in sibling one step behind 
+    for (int i = 1; i < sibling->n; i++) {
+        sibling->keys[i - 1] = sibling->keys[i];
+    }
+
+    // Moving the child pointers one step behind 
+    if (!sibling->leaf) {
+        for (int i = 1; i <= sibling->n; ++i) {
+            sibling->C[i - 1] = sibling->C[i];
+        }
+    }
+
+    // Increasing and decreasing the key count of C[idx] and C[idx+1] 
+    // respectively 
+    child->n++;
+    sibling->n--;
+
+    return;
+}
+
+// A function to merge C[idx] with C[idx+1] 
+// C[idx+1] is freed after merging
+
+void BTreeNode::merge(int idx) {
+    BTreeNode* child = C[idx];
+    BTreeNode* sibling = C[idx + 1];
+
+    // Pulling a key from the current node and inserting it into (t-1)th 
+    // position of C[idx]
+    child->keys[t - 1] = keys[idx];
+
+    // Copying the keys from C[idx+1] to C[idx] at the end 
+    for (int i = 0; i < sibling->n; i++) {
+        child->keys[i + t] = sibling->keys[i];
+    }
+
+    // Copying the child pointers from C[idx+1] to C[idx] 
+    if (!child->leaf) {
+        for (int i = 0; i <= sibling->n; ++i) {
+            child->C[i + t] = sibling->C[i];
+        }
+    }
+
+    // Moving all keys after idx in the current node one step before - 
+    // to fill the gap created by moving keys[idx] to C[idx] 
+    for (int i = idx + 1; i < n; ++i) {
+        keys[i - 1] = keys[i];
+    }
+
+    // Moving the child pointers after (idx+1) in the current node one 
+    // step before
+
+    for (int i = idx + 2; i <= n; ++i) {
+        C[i - 1] = C[i];
+    }
+
+    // Updating the key count of child and the current node 
+    child->n += sibling->n + 1;
+    n--;
+
+    // Freeing the memory occupied by sibling 
+    delete(sibling);
+    return;
+}
 
 
 // Function to traverse all nodes in a subtree rooted with this node 
@@ -454,20 +585,92 @@ void BTreeNode::splitChild(int i, BTreeNode* y)
     n = n + 1;
 }
 
+void BTree::remove(int k) {
+    if (!root) {
+        std::cout << "The tree is empty\n";
+        return;
+    }
+
+    // Call the remove function for root
+    root->remove(k);
+
+    // If the root node has 0 keys, make its first child as the new root 
+    //  if it has a child, otherwise set root as NULL
+    if (root->n == 0) {
+        BTreeNode* tmp = root;
+        if (root->leaf)
+            root = NULL;
+        else
+            root = root->C[0];
+
+        // Free the old root
+        delete tmp;
+    }
+    return;
+}
+
 // Driver program to test above functions 
 int main()
 {
     BTree t(3); // A B-Tree with minium degree 3 
-    t.insert(10);
-    t.insert(20);
-    t.insert(5);
-    t.insert(6);
-    t.insert(12);
-    t.insert(30);
-    t.insert(7);
-    t.insert(17);
 
-    std::cout << "Traversal of the constucted tree is ";
+    t.insert(1);
+    t.insert(3);
+    t.insert(7);
+    t.insert(10);
+    t.insert(11);
+    t.insert(13);
+    t.insert(14);
+    t.insert(15);
+    t.insert(18);
+    t.insert(16);
+    t.insert(19);
+    t.insert(24);
+    t.insert(25);
+    t.insert(26);
+    t.insert(21);
+    t.insert(4);
+    t.insert(5);
+    t.insert(20);
+    t.insert(22);
+    t.insert(2);
+    t.insert(17);
+    t.insert(12);
+    t.insert(6);
+
+    std::cout << "Traversal of tree constructed is\n";
     t.traverse();
+    std::cout << std::endl;
+
+    t.remove(6);
+    std::cout << "Traversal of tree after removing 6\n";
+    t.traverse();
+    std::cout << std::endl;
+
+    t.remove(13);
+    std::cout << "Traversal of tree after removing 13\n";
+    t.traverse();
+    std::cout << std::endl;
+
+    t.remove(7);
+    std::cout << "Traversal of tree after removing 7\n";
+    t.traverse();
+    std::cout << std::endl;
+
+    t.remove(4);
+    std::cout << "Traversal of tree after removing 4\n";
+    t.traverse();
+    std::cout << std::endl;
+
+    t.remove(2);
+    std::cout << "Traversal of tree after removing 2\n";
+    t.traverse();
+    std::cout << std::endl;
+
+    t.remove(16);
+    std::cout << "Traversal of tree after removing 16\n";
+    t.traverse();
+    std::cout << std::endl;
+
     return 0;
 }
